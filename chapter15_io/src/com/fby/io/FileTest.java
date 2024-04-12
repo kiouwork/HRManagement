41164066a8bb4dae12a1e592dc8d825da082f2bc
james <kiouwork@outlook.com>
2024-04-12 09:12:40 +0200

    2024年4月12日测试提交

--- chapter15_io/src/com/fby/io/FileTest.java
+++ chapter15_io/src/com/fby/io/FileTest.java
@@ -1,5 +1,9 @@
 package com.fby.io;
 
+import org.junit.Test;
+
+import java.io.File;
+
 /**
  * ClassName: FileTest
  * Package: com.fby.io
@@ -10,5 +14,29 @@ package com.fby.io;
  * @Version: V0.1
  */
 public class FileTest {
+    @Test
+    public void test1(){
+        //绝对路径
+        File file = new File("D:\\Develop\\Java\\JavaSECode\\chapter15_io\\src\\com\\fby\\io\\123.txt");
+        //相对路径
+        File file1 = new File("abc");
+        System.out.println(file1.exists());
+        System.out.println(file.getAbsolutePath());
+        File file2 = new File("io", "file2");
+        System.out.println(file2.getAbsolutePath());
+        File file3 = new File(file2, "file3.txt");
+        System.out.println(file3.getAbsolutePath());
+    }
 
+    @Test
+    public void test2(){
+        File file = new File("d:\\develop\\java\\javasecode\\chapter15_io\\src\\com\\fby\\io\\123.txt");
+        System.out.println("文件名字:" + file.getName());
+        System.out.println("获取路径" + file.getPath());
+        System.out.println("juedui" + file.getAbsolutePath());
+        System.out.println("wenjian" + file.getAbsoluteFile());
+        System.out.println("up" + file.getParent());
+        System.out.println("size" + file.length());
+        System.out.println("time" + file.lastModified());
+    }
 }
